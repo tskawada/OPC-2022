7881d30415c1448b870303f0352cc07c1519ae3f
S-ryoichi <phdh1ooa@s.okayama-u.ac.jp>
2022-07-11 21:00:19 +0900

    change servo_ctrl func

--- ex1/servo.py
+++ ex1/servo.py
@@ -13,13 +13,10 @@ class Servo:
 
     def servo_ctrl(self, value):
         value = value * 10
-        mod = value % 10
+        print(value)
 
         self.servo.start(0)
-        if(mod != 0 or value > 40 or value < -40):
-            pass
-        else:
-            self.servo_angle(value)
+        self.servo_angle(value)
         self.servo.stop()
 
     def __del__(self):
