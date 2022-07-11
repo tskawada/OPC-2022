479c5534cbc8b2f278ffb1204fbe7a05389a09e6
S-ryoichi <phdh1ooa@s.okayama-u.ac.jp>
2022-07-11 21:16:56 +0900

    move servo.start and servo.stop

--- ex1/servo.py
+++ ex1/servo.py
@@ -6,15 +6,15 @@ class Servo:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(servo_pin, GPIO.OUT)
         self.servo = GPIO.PWM(servo_pin, 50)
+        self.servo.start(0)
 
     def servo_ctrl(self, value):
         value = value * 10
         print(value)
         duty = 2.5 + (12.0 - 2.5) * (value + 90) / 180
 
-        self.servo.start(0)
         self.servo.ChangeDutyCycle(duty)
-        self.servo.stop()
 
     def __del__(self):
+        self.servo.stop()
         GPIO.cleanup()
