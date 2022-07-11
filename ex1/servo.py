444ef957bc93337dcfb647c7a4a404e8876423a6
S-ryoichi <phdh1ooa@s.okayama-u.ac.jp>
2022-07-11 21:02:53 +0900

    change servo_ctrl func

--- ex1/servo.py
+++ ex1/servo.py
@@ -7,16 +7,13 @@ class Servo:
         GPIO.setup(servo_pin, GPIO.OUT)
         self.servo = GPIO.PWM(servo_pin, 50)
 
-    def servo_angle(self, angle):
-        duty = 2.5 + (12.0 - 2.5) * (angle + 90) / 180
-        self.servo.ChangeDutyCycle(duty)
-
     def servo_ctrl(self, value):
         value = value * 10
         print(value)
+        duty = 2.5 + (12.0 - 2.5) * (angle + 90) / 180
 
         self.servo.start(0)
-        self.servo_angle(value)
+        self.servo.ChangeDutyCycle(duty)
         self.servo.stop()
 
     def __del__(self):
