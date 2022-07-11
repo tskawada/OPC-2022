c1a29d7968be12a93c9010a3e27f2b5bcadcdfc3
tskawada <yuta_kawada@techsword.co.jp>
2022-07-11 20:42:41 +0900

    変数名を変更

--- utils/servo.py
+++ utils/servo.py
@@ -5,23 +5,22 @@ class Servo:
         servo_pin = 18
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(servo_pin, GPIO.OUT)
-        self.servo1 = GPIO.PWM(servo_pin, 50)
+        self.servo = GPIO.PWM(servo_pin, 50)
 
     def servo_angle(self, angle):
         duty = 2.5 + (12.0 - 2.5) * (angle + 90) / 180
-        self.servo1.ChangeDutyCycle(duty)
+        self.servo.ChangeDutyCycle(duty)
 
     def servo(self, value):
-        value = value*10
+        value = value * 10
         mod = value % 10
 
-        self.servo1.start(0)
+        self.servo.start(0)
         if(mod != 0 or value > 40 or value < -40):
-            print("Your input angle is mistake!!!!")
-            print()
+            pass
         else:
             self.servo_angle(value)
-        self.servo1.stop()
+        self.servo.stop()
 
     def __del__(self):
         GPIO.cleanup()
